71c7426cad59a07194443b25ac6b6bec651b1741
Rahul Keerthi <rahulkeerthi2@gmail.com>
2020-10-23 16:34:49 +0100

    pug implemented

--- 05_dynamic_content/routes/admin.js
+++ 05_dynamic_content/routes/admin.js
@@ -11,7 +11,7 @@ const products = [] // initialize an empty array for storing products
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
 	res.sendFile(path.join(rootDir, "views", "add-product.html"))
-	res.render("add-product", { docTitle: "Add Product" })
+	res.render("add-product", { docTitle: "Add Product", path: "/admin/add-product" })
 })
 
 // /admin/add-product => POST
--- 05_dynamic_content/routes/shop.js
+++ 05_dynamic_content/routes/shop.js
@@ -9,7 +9,7 @@ const router = express.Router()
 
 router.get("/", (req, res, next) => {
 	const products = adminData.products // we have access to the products array from admin.js
-	res.render("shop", { prods: products, docTitle: "Shop" }) // pass an object with data to render
+	res.render("shop", { prods: products, docTitle: "Shop", path: "/" }) // pass an object with data to render
 	// res.sendFile(path.join(rootDir, "views", "shop.html"))
 })
 
